54f94ebcb103423a82379359773a1e7fb554c6f4
harshitkumar9030 <harshitkumar9030@gmail.com>
2025-03-03 19:07:10 +0530

    removed new year popup.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function HomePage() {
       }
     >
       <HomeClient initialTrack={initialTrack} />
-      <NewYearPopup />
+      {/* <NewYearPopup /> */}
     </Suspense>
   )
 }
